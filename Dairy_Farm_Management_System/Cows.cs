094ddc7242562e039c983c93deb25b39c6dfebb9
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-03-08 17:03:10 +0200

    add ExecuteNonQuery

--- Dairy_Farm_Management_System/Cows.cs
+++ Dairy_Farm_Management_System/Cows.cs
@@ -162,6 +162,8 @@ namespace Dairy_Farm_Management_System
                     con.Open();
                     string Query = "inster into CowTbl values ( '" + CowNameTb.Text + "','" + EarTagTb.Text + "','" + ColorTb.Text + "','" + BreadTb.Text + "','" + AgeTb.Text + "','" + WeigtatBirthTb.Text + "','" + PastureTb.Text + "')";
                     SqlCommand cmd = new SqlCommand( Query,con);
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Cow Saved successdully");
                     con.Close();
                    
                 }catch(Exception ex)
