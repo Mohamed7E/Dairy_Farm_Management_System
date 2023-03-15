5154449cf6ccfed4b16e2b8b3f5e5f90703af70d
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-03-15 17:50:04 +0200

    add connection populate()

--- Dairy_Farm_Management_System/Employee.cs
+++ Dairy_Farm_Management_System/Employee.cs
@@ -14,6 +14,7 @@ namespace Dairy_Farm_Management_System
         public Employee()
         {
             InitializeComponent();
+            populate();
         }
         SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\learn\learn C#\Dairy_Farm_Management_System\data_MOHAMED_EBRAHIM.mdf;Integrated Security=True;Connect Timeout=30");
         private void populate()
