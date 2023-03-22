1ceeef91bc57d546bceb217937e00ac3f699c453
Mohamed7E <m.ibrahim.ict@btu.edu.eg>
2023-03-22 19:35:15 +0200

    add SqlConnection

--- Dairy_Farm_Management_System/login.cs
+++ Dairy_Farm_Management_System/login.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,7 @@ namespace Dairy_Farm_Management_System
         {
             InitializeComponent();
         }
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\learn\learn C#\Dairy_Farm_Management_System\data_MOHAMED_EBRAHIM.mdf;Integrated Security=True;Connect Timeout=30");
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
